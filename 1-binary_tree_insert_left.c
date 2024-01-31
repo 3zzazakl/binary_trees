32971ddd761289bed9ed3e06e04434dfb0403a1d
3zzazakl <vanzekovichgr@gmail.com>
2024-01-31 08:56:29 +0200

    fix task1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,22 +13,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
+	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
 	if (parent->left != NULL)
 	{
-		new_node = binary_tree_node(parent->left, value);
-		if (new_node == NULL)
-			return (NULL);
-
-		parent->left->parent = new_node;
 		new_node->left = parent->left;
-		parent->left = new_node;
+		new_node->left->parent = new_node;
 	}
-	else
-	{
-		new_node = binary_tree_node(parent, value);
-		if (new_node == NULL)
-			return (NULL);
-		parent->left = new_node;
-	}
+
+	parent->left = new_node;
 	return (new_node);
 }
