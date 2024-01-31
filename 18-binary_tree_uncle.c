b7172f4cfecd71ed63466f9fc953b3c78daf5e85
3zzazakl <vanzekovichgr@gmail.com>
2024-01-31 22:54:25 +0200

    fix task18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 
 	if (node->parent == node->parent->parent->left)
-		return (node->parent->parent->left);
+		return (node->parent->parent->right);
 
-	return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
